41312b4b19130f232645ff725f2e91b84f923414
Miguel Angel <histonzy@gmail.com>
2016-04-25 13:13:18 +0200

    test 1

---
+++ WebProject v2/Infraestructura/IRepositoryCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Domain;
+
+namespace Infraestructura
+{
+    public interface IRepositoryCustomer : IUnitOfWork
+    {
+        IDbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ WebProject v2/Services/ServiceBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Infraestructura;
+
+namespace Services
+{
+    public class ServiceBase //: IDisposable
+    {
+        readonly IUnitOfWork _unitOfWork;
+        public ServiceBase()
+        {
+
+        }
+    }
+}
