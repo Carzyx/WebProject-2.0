572aa475c7b9f95e6e593e54b9d7e9e1849c768a
carolgaloy <carolina.loygorri@gmail.com>
2016-04-25 17:41:28 +0200

    Commit que ya no recuerdo que era xD

--- WebProject v2/WebProject/App_Start/RouteConfig.cs
+++ WebProject v2/WebProject/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Customer", action = "Create", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
